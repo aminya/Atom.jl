676f0427bf990829bbef38c0a0a7a92e8c41bc9f
aminya <aminyahyaabadi74@gmail.com>
2020-01-26 17:54:58 -0600

    precompile goto and modules

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -3,22 +3,22 @@ using SnoopCompile
 # using runtests except modules/goto
 ################################################################
 # disabling inlcude modules/goto
-a1 = "include(\"modules.jl\")"
-a2 = "include(\"goto.jl\")"
+# a1 = "include(\"modules.jl\")"
+# a2 = "include(\"goto.jl\")"
 
-testText = Base.read("test/runtests.jl", String)
-testEdited = foldl(replace,
-                     (
-                      a1 => "#"*a1,
-                      a2 => "#"*a2,
-                     ),
-                     init = testText)
-Base.write("test/runtests.jl", testEdited)
+# testText = Base.read("test/runtests.jl", String)
+# testEdited = foldl(replace,
+#                      (
+#                       a1 => "#"*a1,
+#                       a2 => "#"*a2,
+#                      ),
+#                      init = testText)
+# Base.write("test/runtests.jl", testEdited)
 ################################################################
 @snoopiBot BotConfig("Atom", blacklist = ["realpath′"])
 ################################################################
 # enabling back inlcude modules/goto
-Base.write("test/runtests.jl", testText)
+# Base.write("test/runtests.jl", testText)
 
 
 ################################################################
