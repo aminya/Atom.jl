9ef442b2e5e21d4d787d4f7ec789e46f99800f02
aminya <aminyahyaabadi74@gmail.com>
2020-04-18 19:33:59 -0500

    Add versions and os

--- deps/SnoopCompile/snoopi_bot.jl
+++ deps/SnoopCompile/snoopi_bot.jl
@@ -1,3 +1,3 @@
 using SnoopCompile
 
-@snoopi_bot BotConfig("Atom", blacklist = ["realpath′","restart_copyto_nonleaf!","allocatedinline"])
+@snoopi_bot BotConfig("Atom", blacklist = ["realpath′"], os = ["windows", "linux", "apple"], version = [v"1.4.1", v"1.3.1", v"1.2.0", v"1.0.5"])
